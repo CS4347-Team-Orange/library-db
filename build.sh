1e94577d33ccc47b210ce0ade0b5de8e91802424
Alex Schittko <aschittko@thrivepos.com>
2021-07-02 05:14:16 -0500

    Update buildscript

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 
 set -e # Abort on any error 
 
-PROJECT_NAME="" # Set this to the repo's name
-
-# Do what you need to do to prepare for the container's build here.
-# Compile your code, run unit tests, etc.
+PROJECT_NAME="db" # Set this to the repo's name
 
 # Build the container
-docker build -t library:${project_name} .
+docker build -t library/${project_name} .
+
