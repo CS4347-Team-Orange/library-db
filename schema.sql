6963c85352c720e669277973ca344ef11a44f155
Arunima Gulia <axg174830@utdallas.edu>
2021-07-19 23:12:12 -0500

    Updating schema

--- schema.sql
+++ schema.sql
@@ -1,38 +1,50 @@
 CREATE TABLE IF NOT EXISTS test (
-	id INT PRIMARY KEY
+    id INT PRIMARY KEY
 );
 CREATE TABLE IF NOT EXISTS BOOK (
-	isbn bigint PRIMARY KEY,
-	title TEXT
+    isbn bigint PRIMARY KEY,
+    title TEXT
 );
 CREATE TABLE IF NOT EXISTS AUTHORS (
-	author_id SERIAL PRIMARY KEY,
-	name TEXT
+    author_id SERIAL PRIMARY KEY,
+    name TEXT
 );
 CREATE TABLE IF NOT EXISTS BOOK_AUTHORS (
-	author_id SERIAL, -- TODO FK to AUTHORS
-	isbn bigint -- TODO FK to BOOK
+    author_id SERIAL,
+    CONSTRAINT book_author_authorid
+    FOREIGN KEY(author_id)
+    REFERENCES AUTHORS(author_id),
+    isbn bigint,
+    PRIMARY KEY(author_id, isbn),
+    CONSTRAINT book_author_isbn
+      FOREIGN KEY(isbn)
+      REFERENCES BOOK(isbn)
+      ON DELETE SET NULL
 );
--- TODO Composite key on BOOK_AUTHORS of both columns
+
 CREATE TABLE IF NOT EXISTS BORROWER (
-	card_id TEXT PRIMARY KEY DEFAULT uuid_generate_v1(),
-	ssn BIGINT, -- TODO Unique constraint
-	bname TEXT,
-	address TEXT,
-	phone TEXT
+    card_id TEXT PRIMARY KEY DEFAULT uuid_generate_v1(),
+    ssn BIGINT UNIQUE,
+    bname TEXT,
+    address TEXT,
+    phone TEXT
 );
 CREATE TABLE IF NOT EXISTS BOOK_LOANS (
-	loan_id UUID PRIMARY KEY,
-	isbn bigint, -- TODO FK to BOOK
-	card_id TEXT, -- TODO FK to BORROWER
-	date_out date, -- TODO Default today
-	due_date date, -- TODO Default Date_out + 14 days
-	date_in date -- TODO default NULL
+    loan_id UUID PRIMARY KEY,
+    isbn bigint,
+    CONSTRAINT book_loan_isbn
+    FOREIGN KEY(isbn)
+    REFERENCES BOOK(isbn),
+    card_id TEXT,
+    CONSTRAINT borrower_card_id
+    FOREIGN KEY(card_id)
+    REFERENCES BORROWER(card_id),
+    date_out date not null default CURRENT_DATE,
+    due_date date not null default CURRENT_DATE + INTERVAL '14 days',
+    date_in date default NULL
 );
--- TODO INSERT Constraint for BOOK_LOANS
--- Number of BOOK_LOANS may not be > 3 for the same CARD_ID
 CREATE TABLE IF NOT EXISTS FINES (
-	loan_id UUID PRIMARY KEY,
-	fine_amt decimal,
-	paid boolean
+    loan_id UUID PRIMARY KEY,
+    fine_amt decimal,
+    paid boolean
 );
