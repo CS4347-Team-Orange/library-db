2e655738660f451b8ffc60deba33504b07ca6928
Alex Schittko <aschittko@thrivepos.com>
2021-07-30 03:02:54 -0500

    Normalize inserts authors now

--- normalize.py
+++ normalize.py
@@ -1,13 +1,38 @@
 import csv
+import psycopg2
 
+insert_author_query = """
+INSERT INTO
+    AUTHORS (name)
+VALUES
+    (%s)
+"""
+
+## Load the authors
 tsv_file = open('books.tsv')
-author_data = csv.reader(tsv_file, delimiter="\t")
-list = []
-for a in author_data:
-    names = a[3].split(",")
+lines = csv.reader(tsv_file, delimiter="\t")
+authorsList = []
+for line in lines:
+    names = line[3].split(",")
     for name in names:
         if name != 'author':
             name = name.strip()
-            list.append(name)
-print (list)
+            authorsList.append(name)
 
+## Insert each author to the database
+conn = None
+conn = psycopg2.connect(dbname="library", user="library", password="library", host="localhost")
+cur = conn.cursor()
+
+for author in authorsList:
+    try:   
+        cur.execute(insert_author_query, [author])
+        conn.commit()
+    except (psycopg2.errors.UniqueViolation, psycopg2.errors.InFailedSqlTransaction):
+        conn.rollback()
+
+print("Inserted authors")
+
+## Iterate each author in db
+
+#### Link to books
