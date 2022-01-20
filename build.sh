aa3b0cb219c1cbf5ca6f9506896ec3cf7d895172
Alex Schittko <alex4108@live.com>
2022-01-20 02:05:30 -0600

    add build.sh output

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ if [[ "${container_image}" == "" && "${TRAVIS_COMMIT}" == "" ]]; then
     container_image="alex4108/library-db"
 fi
 
+echo "Building image: ${container_image}"
+
 docker build -t ${container_image} .
 
